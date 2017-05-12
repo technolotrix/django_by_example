d130c1a54d1ff3a49cc87c791b229b2b778a4683
technolotrix <technolotrix@gmail.com>
2017-05-12 12:10:42 -0400

    login and log out

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # previous login view
+    # url(r'^login/$', views.user_login, name='login'),
+
+    # login / logout urls
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login'),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout'),
+    url(r'^logout-then-login/$',
+        'django.contrib.auth.views.logout_then_login',
+        name='logout_then_login'),
+    url(r'^$', views.dashboard, name='dashboard'),
+]

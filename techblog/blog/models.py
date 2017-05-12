37e8d44e1978e5201d3bcd0941eb8ef7cae7b9a1
technolotrix <technolotrix@gmail.com>
2017-05-11 20:27:58 -0400

    created templates for views, fixed bug

--- techblog/blog/models.py
+++ techblog/blog/models.py
@@ -2,9 +2,26 @@ from django.db import models
 
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+            self).get_queryset()\
+                .filter(status='published')
 
 
 class Post(models.Model):
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.strftime('%m'),
+                self.publish.strftime('%d'),
+                self.slug])
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -25,24 +42,13 @@ class Post(models.Model):
         choices=STATUS_CHOICES,
         default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
 
 class Meta:
     ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager,
-            self).get_queryset()\
-                .filter(status='published')
-
-
-class Post(models.Model):
-    # ...
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
-
 
--- techblog/techblog/urls.py
+++ techblog/techblog/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls',
+        namespace='blog',
+        app_name='blog')),
 ]

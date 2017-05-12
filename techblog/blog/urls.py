763ff687db09e7504dc1bd9fb67d8410ee71228b
technolotrix <technolotrix@gmail.com>
2017-05-11 20:49:57 -0400

    convert post view to object, finish chapter 1

--- techblog/blog/urls.py
+++ techblog/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # post views
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
         r'(?P<post>[-\w]+)/$',
         views.post_detail,

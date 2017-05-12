ba08b8bf89747812b28d30269ad044a215217ad4
technolotrix <technolotrix@gmail.com>
2017-05-11 22:20:17 -0400

    adding comments

---
+++ techblog/blog/migrations/0002_comment.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=80)),
+                ('email', models.EmailField(max_length=254)),
+                ('body', models.TextField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('active', models.BooleanField(default=True)),
+                ('post', models.ForeignKey(to='blog.Post', related_name='comments')),
+            ],
+            options={
+                'ordering': ('created',),
+            },
+        ),
+    ]
--- techblog/blog/views.py
+++ techblog/blog/views.py
@@ -4,8 +4,8 @@ from django.core.paginator import Paginator, EmptyPage,\
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -21,9 +21,25 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day)
 
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
         'blog/post/detail.html',
-        {'post': post})
+            {'post': post, 'comments': comments,
+                'comment_form': comment_form})
 
 def post_share(request, post_id):
     # Retrieve post by id

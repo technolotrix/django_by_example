2a1fcc7bf9741143d24d7acbb320ce4c26276009
technolotrix <technolotrix@gmail.com>
2017-05-13 20:58:02 -0400

    create base shop

---
+++ myshop/shop/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(unique=True, max_length=200)),
+            ],
+            options={
+                'ordering': ('name',),
+                'verbose_name': 'category',
+                'verbose_name_plural': 'categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
+                ('image', models.ImageField(blank=True, upload_to='products/%Y/%m/%d')),
+                ('description', models.TextField(blank=True)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
+                ('stock', models.PositiveIntegerField()),
+                ('available', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(to='shop.Category', related_name='products')),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+        migrations.AlterIndexTogether(
+            name='product',
+            index_together=set([('id', 'slug')]),
+        ),
+    ]

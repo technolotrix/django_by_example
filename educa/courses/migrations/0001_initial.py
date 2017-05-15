b5cb144a9bb0715e7f96a7a8180a3b5a212ec75a
technolotrix <technolotrix@gmail.com>
2017-05-15 10:34:38 -0400

    initial setup, fixtures

---
+++ educa/courses/migrations/0001_initial.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Course',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+                ('overview', models.TextField()),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='courses_created')),
+            ],
+            options={
+                'ordering': ('-created',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Module',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('description', models.TextField(blank=True)),
+                ('course', models.ForeignKey(to='courses.Course', related_name='modules')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Subject',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+            ],
+            options={
+                'ordering': ('title',),
+            },
+        ),
+        migrations.AddField(
+            model_name='course',
+            name='subject',
+            field=models.ForeignKey(to='courses.Subject', related_name='courses'),
+        ),
+    ]
